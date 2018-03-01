63bdfaf16c5526ebf5ef040150fc3ddfa726af55
peeta2 <36959446+peeta2@users.noreply.github.com>
2018-03-01 15:40:04 +0100

    Update Application.java

--- src/main/java/cz/peeta2/employeedatabase/Application.java
+++ src/main/java/cz/peeta2/employeedatabase/Application.java
@@ -3,14 +3,7 @@ package cz.peeta2.employeedatabase;
 public class Application {
 
 	public static void main(String[] args) {
-		EmployeeDatabase employeeDatabase;
-		try {
-			employeeDatabase = new EmployeeDatabaseFactory().getSampleEmployeeDatabase();
-		} catch (Exception e) {
-			employeeDatabase = new EmployeeDatabase();
-		}
-		CLI cli = new CLI(employeeDatabase);
-		
+		CLI cli = new CLI(new EmployeeDatabase());
 		cli.start();
 	}
 }
